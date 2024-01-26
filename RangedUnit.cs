a9c75e8e316151808678d9cff4084ff8aef9f973
Noga25 <nogale40@gmail.com>
2024-01-26 23:51:48 +0200

    Define defult

--- RangedUnit.cs
+++ RangedUnit.cs
@@ -6,8 +6,8 @@ namespace Brezerkers
 {
     public abstract class RangedUnit : Unit
     {
-        //Propetie who set the range
-        protected abstract float Range { get; }
+        //Propetie who set the range / has defult range
+        protected virtual float Range { get; } = 10;
 
         public virtual bool CheckRange(Unit target, int TargetDistance)
         {
